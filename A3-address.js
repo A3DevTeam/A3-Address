3eebcbffe615e7d7ce6946a3fa8fba91f3994905
kkho4043 <kkho4043@naverr.com>
2023-04-26 16:01:40 +0900

    input mode modify

--- A3-address.js
+++ A3-address.js
@@ -268,8 +268,7 @@
                 $A3(inputid).attr("spellcheck" , "off")
                 $A3(inputid).attr("autocapitalize" , "off")
                 $A3(inputid).attr("aria-autocomplete" , "both")
-                $A3(inputid).attr("inputmode" , "search")
-
+            
                 $A3(inputid).autocomplete({
                     source: function (request, response) {
                         $A3.ajax({
